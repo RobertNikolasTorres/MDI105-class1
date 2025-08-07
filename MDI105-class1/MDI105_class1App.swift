c2b6d5a43e8a7c94b4e2d3c75e4eea9151266376
robert <robert.nikolas.torres@gmail.com>
2025-08-06 21:00:47 -0400

    Initial Commit

---
+++ MDI105-class1/MDI105_class1App.swift
@@ -0,0 +1,17 @@
+//
+//  MDI105_class1App.swift
+//  MDI105-class1
+//
+//  Created by Robert Nikolas Torres on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MDI105_class1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
